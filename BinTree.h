f147e708a49bc2a4d5abd354761d95c6bdb3c711
12UE <1258432472@qq.com>
2022-08-12 19:57:15 +0800

    更新

--- BinTree.h
+++ BinTree.h
@@ -22,17 +22,68 @@ namespace dsa {
 	             int h = 0, int l = 1, RBColor c = RB_RED) :
 	      data(e), parent(p), lc(lc), rc(rc), height(h), npl(l), color(c) { }
 	// 操作接口
-	   int size(); //统计当前节点后代总数，亦即以其为根的子树的规模
-	   BinNodePosi<T> insertAsLC(T const&); //作为当前节点的左孩子插入新节点
-	   BinNodePosi<T> insertAsRC(T const&); //作为当前节点的右孩子插入新节点
-	   BinNodePosi<T> succ(); //取当前节点的直接后继
-	   template <typename VST> void travLevel(VST&); //子树层次遍历
-	   template <typename VST> void travPre(VST&); //子树先序遍历
-	   template <typename VST> void travIn(VST&); //子树中序遍历
-	   template <typename VST> void travPost(VST&); //子树后序遍历
+	   int size(); //统计当前节点后代总数，亦即以其为根的子树的规模    <------[后代总数]
+	   BinNodePosi<T> insertAsLC(T const& e); //作为当前节点的左孩子插入新节点
+	   BinNodePosi<T> insertAsRC(T const& e); //作为当前节点的右孩子插入新节点
+	   BinNodePosi<T> succ(); //取当前节点的直接后继 //中序遍历下的直接后继
+	   template <typename VST> void travLevel(VST& fun); //子树层次遍历
+	   template <typename VST> void travPre(VST& fun); //子树先序遍历
+	   template <typename VST> void travIn(VST& fun); //子树中序遍历
+	   template <typename VST> void travPost(VST& fun); //子树后序遍历
 	// 比较器、判等器（各列其一，其余自行补充）
 	   bool operator< (BinNode const& bn) { return data < bn.data; } //小于
 	   bool operator== (BinNode const& bn) { return data == bn.data; } //等于
 	};
 	
+	template<typename T>
+	inline int BinNode<T>::size(){//时间复杂度 O(n) 线性的时间
+		int s = 1;
+		if(lc) s += lc->size();//递归
+		if(rc) s += rc->size();
+		return s;
+	}
+
+	template<typename T>
+	inline BinNodePosi<T> BinNode<T>::insertAsLC(T const& e)
+	{
+		return lc = new BinNode(e, this);
+	}
+
+	template<typename T>
+	inline BinNodePosi<T> BinNode<T>::insertAsRC(T const& e)
+	{
+		return rc = new BinNode(e, this);
+	}
+	template <typename T> class BinTree { //二叉树模板类
+	protected:
+	   int _size; BinNodePosi<T> _root; //规模、根节点
+	   virtual int updateHeight(BinNodePosi<T> x); //更新节点x的高度
+	   void updateHeightAbove(BinNodePosi<T> x); //更新节点x及其祖先的高度
+	public:
+	   BinTree() : _size(0), _root(NULL) { } //构造函数
+	   ~BinTree() { if (0 < _size) remove(_root); } //析构函数
+	   int size() const { return _size; } //规模
+	   bool empty() const { return !_root; } //判空
+	   BinNodePosi<T> root() const { return _root; } //树根
+	   BinNodePosi<T> insert(T const&); //插入根节点
+	   BinNodePosi<T> insert(T const&, BinNodePosi<T>); //插入左孩子
+	   BinNodePosi<T> insert(BinNodePosi<T>, T const&); //插入右孩子
+	   BinNodePosi<T> attach(BinTree<T> *&, BinNodePosi<T>); //接入左子树
+	   BinNodePosi<T> attach(BinNodePosi<T>, BinTree<T> *&); //接入右子树
+	   int remove(BinNodePosi<T>); //子树删除
+	   BinTree<T>* secede(BinNodePosi<T>); //子树分离
+	   template < typename VST> //操作器
+	   void travLevel(VST & visit) { if (_root) _root->travLevel(visit); } //层次遍历
+	   template < typename VST> //操作器
+	   void travPre(VST & visit) { if (_root) _root->travPre(visit); } //先序遍历
+	   template < typename VST> //操作器
+	   void travIn(VST & visit) { if (_root) _root->travIn(visit); } //中序遍历
+	   template < typename VST> //操作器
+	   void travPost(VST & visit) { if (_root) _root->travPost(visit); } //后序遍历
+	   bool operator< (BinTree<T> const& t) //比较器（其余自行补充）
+	   { return _root && t._root && lt(_root, t._root); }
+	   bool operator== (BinTree<T> const& t) //判等器
+	   { return _root && t._root && (_root == t._root); }
+	
+	}; //BinTree
 }
